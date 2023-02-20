a35d9155b7ffef72e9c69ea473a1b0fc437bb472
dicey <esanelijah@icloud.com>
2023-02-20 10:40:57 -0800

    done with the layout yet to fix some minor things

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Header from "./components/Header/Header";
 import Hero from "./components/Hero/Hero";
+import Build from "./components/Build/Build";
 
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
       <div className="xl:max-w-[1280px] mx-auto">
         <Header />
         <Hero />
+        <Build />
       </div>
     </div>
   )
