0626223cf940aa41a4e72aacf9ab0cce26870349
daddyloyal <lifeofemmany6@gmail.com>
2023-02-23 04:18:11 +0100

    Done with the navbar, my God I'm So happy!!!

--- src/constants/static.js
+++ src/constants/static.js
@@ -1,3 +1,19 @@
+import HeartIcon from "../assets/heart_icon.svg";
+import PricingIcon from "../assets/pricing.svg";
+import MovieIcon from "../assets/movie_icon.svg";
+import Connect from "../assets/connect.svg";
+import Statement from "../assets/statement-pages.svg";
+import DirectPay from "../assets/directpay.svg";
+import DirectPayPages from "../assets/direct-pay-pages.svg";
+import Portal from "../assets/portal.svg";
+import Percepts from "../assets/percept.svg";
+import About from "../assets/about-us-icon.svg";
+import Blog from "../assets/blog-icon.svg";
+import Tutorials from "../assets/tutorials-icon.svg";
+import Coverage from "../assets/product-icon.svg";
+import Overview from "../assets/overview-icon.svg";
+import Documentation from "../assets/docs-icon.svg";
+
 export const navLinks = [
   {
     id: "why-us",
@@ -14,5 +30,90 @@ export const navLinks = [
   {
     id: "developers",
     title: "Developers",
+  },
+];
+
+export const whyMono = [
+  {
+    id: 'why-mono',
+    title: 'Why Mono',
+    img: HeartIcon,
+  },
+  {
+    id: 'pricing',
+    title: 'Pricing',
+    img: PricingIcon,
+  },
+  {
+    id: 'demo',
+    title: 'See a Demo',
+    img: MovieIcon,
+  },
+];
+export const ourProducts = [
+  {
+    id: 'connect',
+    title: 'Connect',
+    img: Connect,
+  },
+  {
+    id: 'statement',
+    title: 'Statement Pages',
+    img: Statement,
+  },
+  {
+    id: 'direct_pay',
+    title: 'DirectPay',
+    img: DirectPay,
+  },
+  {
+    id: 'direct_pay_pages',
+    title: 'DirectPay Pages',
+    img: DirectPayPages,
+  },
+  {
+    id: 'portal',
+    title: 'Portal',
+    img: Portal,
+  },
+  {
+    id: 'percept',
+    title: 'Percept',
+    img: Percepts,
+  },
+];
+
+export const Learn = [
+  {
+    id: 'about',
+    title: 'About Us',
+    img: About,
+  },
+  {
+    id: 'blog',
+    title: 'Blog',
+    img: Blog,
+  },
+  {
+    id: 'tutorials',
+    title: 'Tutorials',
+    img: Tutorials,
+  },
+  {
+    id: 'coverage',
+    title: 'Coverage',
+    img: Coverage,
+  }
+];
+export const Developers = [
+  {
+    id: 'overview',
+    title: 'Overview',
+    img: Overview,
+  },
+  {
+    id: 'documentation',
+    title: 'Documentation',
+    img: Documentation,
   },
 ];
